7da55f4d44b9aae69c5106894c48b9aafb69d421
Phil <phil.pichard@orange.fr>
2025-02-17 11:42:54 +0100

    Release

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
 # yaml2env
-Convert the whole or a specific branch of a YAML file to the ENV format.
+A single file python script to convert the whole or a specific branch of a YAML file to the ENV format.
+
+# Usage
+yaml2env.py [-h] [--parents | --no-parents] [--children | --no-children] filename path
+
+By default, children is set to false and parents is set to true. These default settings flatten a branch from the root to the node.
+
+<b>filename</b> : 
+
+The YAML file to parse.
+
+<b>path</b> :
+
+The path to a node in the YAML file. The diffent levels are separated by a '#' and the first one could be omitted.
+
+'foo#bar#baz' is equivalent to '#foo#bar#baz and represents
+<pre>
+.
+├── foo
+│   ├── bar
+│   │   ├── baz
+</pre>
+
+## CLI examples
+
+```
+yaml2env.py file.yaml 'foo#bar'
+```
+
+Returns entries of each levels root, 'foo' and 'bar' without including deeper levels like 'baz'.
+
+
+```
+yaml2env.py --children file.yaml ''
+```
+
+Flatten the whole file.
+
+
+```
+yaml2env.py --no-parents --children file.yaml 'foo#bar'
+```
+
+Returns all entries of 'bar' and its children.
+
+
+## Errors
+- If an incorrect path is given, the script will stop with an error message.
+- If duplicate names are found, the script will warn you, keep one of the values encountered and continue.
+- If invalid ENV names are found, the script will warn you, discard the entrie and continue.
---
+++ yaml2env.py
@@ -0,0 +1,71 @@
+#!/bin/python3
+
+import argparse
+import re
+import sys
+import yaml
+
+valid_var_name = re.compile(r'^[a-zA-Z_]+[a-zA-Z_0-9]*$')
+
+def scalars(node: dict) -> dict[str, any]:
+    return {k:v for k, v in node.items() if isinstance(v, (str, int, float, bool))}
+
+def children_nodes(node:dict) -> list[str]:
+    return [k for k, v in node.items() if not isinstance(v, (str, int, float, bool))]
+
+def walk(node, path, parents, children, depth=0):
+    max_depth = len(path) - 1
+    curr_path = ' > '.join(['root'] + path[1:depth+1])
+
+    values = {}
+    if parents or depth == max_depth or depth > max_depth and children:
+        entries = scalars(node)
+        valid_entries = {k:v for k,v in entries.items() if valid_var_name.match(k)}
+        if len(valid_entries) != len(entries):
+            invalid = set(entries.keys()) - set(valid_entries.keys())
+            print(f"Invalid env var name found in {curr_path} : {list(invalid)}", file=sys.stderr)
+        values.update({k.upper():v for k,v in valid_entries.items()})
+        
+    branches = children_nodes(node)
+    if depth < max_depth:
+        if path[depth+1] in branches:
+            branches = [path[depth+1]]
+        else:
+            print(f"Path not found : {curr_path}", file=sys.stderr)
+            exit(1)
+    else:
+        if depth == max_depth and not children:
+            branches = []
+            
+    for name in branches:
+        if name == "":
+            continue
+        sub_node = node[name]
+        if not isinstance(sub_node, dict):
+            print(f"Expected a branch but found a node in {curr_path} : {' > '.join(branches[:depth+1])}", file=sys.stderr)
+            exit(1)
+        new_entries = walk(sub_node, path if depth < max_depth else path + [name], parents, children, depth+1)        
+        if len(dups := set(values.keys()) & set(new_entries.keys())) > 0:
+            print(f"Duplicate keys in {curr_path} : {dups}", file=sys.stderr)
+        values.update(new_entries)
+
+    return values
+
+
+parser = argparse.ArgumentParser(description="")
+parser.add_argument('filename', help="YAML filename")
+parser.add_argument('path', help="Path to the YAML node. Use # as separator")
+parser.add_argument('--parents', action=argparse.BooleanOptionalAction, default=True, help="Include parents' vars")
+parser.add_argument('--children', action=argparse.BooleanOptionalAction, default=False, help="Include children's vars")
+args = parser.parse_args()
+
+with open(args.filename) as f:
+    node = yaml.safe_load(f)
+
+    path = args.path.strip().split('#')
+    path = [p for i,p in enumerate(['']+path) if i==0 or p.strip() != "" ]
+
+    vars = walk(node, path, args.parents, args.children)
+    escaped_vars = {k:v if not isinstance(v, str) else "'{}'".format(v.replace("'", "\\'")) for k,v in vars.items()}
+    print("\n".join([f'{k}={v}' for k,v in escaped_vars.items()]))    
+        
